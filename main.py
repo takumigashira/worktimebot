46cd4d3f4d30d3103d7b6854495115c7bc29ef6e
takumigashira <takumigashira@gmail.com>
2020-04-30 17:13:12 +0900

    add Procfile requirements.txt

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.reply_token == "00000000000000000000000000000000":
+        return
 	line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='「' + event.message.text + '」って何？')
